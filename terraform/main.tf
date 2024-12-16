91af38950240f4bdc011d7d593b370797010a078
Dhanika Kumarasiri <dhanikaanupama2000@gmail.com>
2024-12-17 00:36:24 +0530

    Created terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,99 @@
+terraform{
+    required_providers {
+      aws = {
+        source = "hashicorp/terraform"
+        version = "~>4.0"
+      }
+    }
+    backend "s3" {
+        key = "aws/ec2-deploy/terraform.tfstate"
+    }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "server" {
+  ami = "ami-05edb7c94b324f73c"
+  instance_type = "t3.micro"
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.mainGroup.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = "ubuntu"
+    private_key = var.private_key
+    timeout = "4m"
+  }
+  tags {
+    name = "deployVM"
+  }
+}
+
+resource "aws_iam_instance_profile" "ec2-profile" {
+  name = "ec2-profile"
+  role = "ec2-ecr-auth"
+}
+
+resource "aws_security_group" "mainGroup" {
+  egress = [
+    {
+        cidr_blocks = ["0.0.0.0/0"]
+        description = ""
+        from_port = 0
+        ipv6_cidr_blocks = []
+        prefix_list_ids = []
+        protocol = "-1"
+        security_groups = []
+        self = false
+        to_port = 0
+    }
+  ]
+  ingress = [
+    {
+        cidr_blocks = ["0.0.0.0/0"]
+        description = ""
+        from_port = 22
+        ipv6_cidr_blocks = []
+        prefix_list_ids = []
+        protocol = "tcp"
+        security_groups = []
+        self = false
+        to_port = 22
+    },
+    {
+        cidr_blocks = ["0.0.0.0/0"]
+        description = ""
+        from_port = 80
+        ipv6_cidr_blocks = []
+        prefix_list_ids = []
+        protocol = "tcp"
+        security_groups = []
+        self = false
+        to_port = 80
+    },
+    {
+        cidr_blocks = ["0.0.0.0/0"]
+        description = ""
+        from_port = 80
+        ipv6_cidr_blocks = []
+        prefix_list_ids = []
+        protocol = "tcp"
+        security_groups = []
+        self = false
+        to_port = 443
+    }
+  ]
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name = var.key_name
+  public_key = var.public_key
+}
+
+output "instance_public_ip" {
+  value = aws_instance.server.public_ip
+  sensitive = true
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default = "eu-north-1"
+}
+variable "public_key" {
+}
+variable "private_key" {  
+}
+variable "key_name" { 
+}
