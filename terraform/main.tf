9fef3b1a505f38c7eff7969262d268186324eb5a
Dhanika Kumarasiri <dhanikaanupama2000@gmail.com>
2024-12-17 04:11:49 +0530

    Made simple changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "server" {
-  ami                    = "ami-05edb7c94b324f73c"
+  ami                    = "ami-075449515af5df0d1"
   instance_type          = "t3.micro"
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.mainGroup.id]
